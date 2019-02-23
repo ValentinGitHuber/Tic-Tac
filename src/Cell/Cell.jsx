e6a071bc10f978a20c3b83b0de343ddddb8e9996
Valentin <perlogv@gmail.com>
2019-02-23 18:36:53 +0200

    core game

--- src/Cell/Cell.jsx
+++ src/Cell/Cell.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 import './Cell.css';
-import PropTypes from 'prop-types';
 import Piece from '../Piece/Piece';
 
 class Cell extends Component {
