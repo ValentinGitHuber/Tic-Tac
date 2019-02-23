62c495d86d8385e5a353c30eb6f99d6d302cef80
Valentin <perlogv@gmail.com>
2019-02-23 17:12:54 +0200

    move

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import './App.css';
 import Cell from './Cell/Cell';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    /////////////////////////////
     // Tic Tac deafult game rules
+    /////////////////////////////
     // Starting default side
-    const starts = 1;
-    // Positions are table cells
-    const positions = () => {
+    const startingSide = 1;
+    // Positions are table cells with their ids
+    const positions = () => { //array
       return new Array(9).fill(null).map((x, i) => {
         return {id: i};
       });
     }
-    // Avaible pieces to put on table
-    const pieces = (starts) => {
+    // Avaible pieces to put on table with their side
+    const pieces = (startingSide) => { //array
       return new Array(9).fill(null).map((x, i) => {
-        switch (starts) {
+        switch (startingSide) {
           case 1:
-            return {side: i < 5 ? 1 : 0};
-          default:
             return {side: i < 5 ? 0 : 1};
+          default:
+            return {side: i < 5 ? 1 : 0};
         }
       });
     };
@@ -31,11 +34,69 @@ class App extends Component {
       positions: positions(),
       pieces: pieces(),
       game: {
-        starts: starts,
+        movingSide: startingSide,
         win: null,
         loose: null
       }
     };
+
+    // HandleCliks
+    this.getClickedId = this.getClickedId.bind(this);
+  }
+
+  getClickedId(id) {
+    console.log('Clicked:', id)
+
+    // Validate if clicked id is avaible for placing piece
+    const validate = this.validatePosition(id);
+    console.log('Position valid: ', validate);
+    if (validate) {
+      this.move(id);
+    }
+    
+    
+  }
+
+  validatePosition(id) {//true if avaible
+    const positions = this.state.positions;
+    const position = _.find(positions, ['id', id]);
+    return !_.has(position, 'piece');
+  }
+
+  move(id) {
+    const movingSide = parseInt(this.state.game.movingSide);
+    console.log('Moving side: ', movingSide)
+    // Remove piece from pieces by movingSide
+    const pieces = this.state.pieces;
+    console.log('pieces: ', pieces)
+    const index = _.findIndex(pieces, { side: movingSide });
+    console.log('index ', index)
+    let newpieces = pieces;
+    if (index > -1) {
+      newpieces =  [...pieces.slice(0,index), ...pieces.slice(index+1)];
+      console.log('newpieces ', newpieces)
+    }
+    
+
+
+    // Set piece to position
+    const positions = this.state.positions;
+    const position = _.find(positions, ['id', id]);
+    position.piece = {
+      side: movingSide
+    };
+    // Revert movingSide
+    const game = this.state.game;
+    game.movingSide = movingSide === 0 ? 1 : 0;
+    
+
+    this.setState({
+      positions,
+      pieces: newpieces,
+      game
+    });
+
+    // console.log(game)
   }
 
 
@@ -43,7 +104,7 @@ class App extends Component {
   render() {
     let cells = () => {
       return this.state.positions.map((item, i) => {
-        return (<Cell key={i}/>)
+        return (<Cell key={i} id={item.id} sendId={this.getClickedId}/>)
       });
     }
 
--- src/Cell/Cell.jsx
+++ src/Cell/Cell.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import './Cell.css';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Cell extends Component {
+    constructor(props) {
+        super(props);
+        // HandleClicks
+        this.clickCell = this.clickCell.bind(this);
+    }
+
+    clickCell() {
+        this.props.sendId(this.props.id);
+    }
+
     render() {
         return (
-            <div className="Cell">
-                Cell
+            <div className="Cell" onClick={this.clickCell}>
+                {this.props.id}
             </div>
         );
     }
 }
 
-/* Cell.propTypes = {
-    name: PropTypes.string
-  }; */
+Cell.propTypes = {
+    id: PropTypes.number
+};
 
 export default Cell;
