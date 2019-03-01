6e9fb2e7abaf3a718251d55fc2ac0958da70466d
Valentin <perlogv@gmail.com>
2019-03-02 00:04:41 +0200

    Updates

--- precache-manifest.9eb012840443bb6acb05fc4b6f55d5b2.js
+++ precache-manifest.9eb012840443bb6acb05fc4b6f55d5b2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/tic-tac/static/js/runtime~main.b528fe44.js"
   },
   {
-    "revision": "dc16a81069a6ff15748e",
-    "url": "/tic-tac/static/js/main.dc16a810.chunk.js"
+    "revision": "f3e8cbfd5ace99237e90",
+    "url": "/tic-tac/static/js/main.f3e8cbfd.chunk.js"
   },
   {
     "revision": "6c7702c29bd252f9ff75",
     "url": "/tic-tac/static/js/2.6c7702c2.chunk.js"
   },
   {
-    "revision": "dc16a81069a6ff15748e",
+    "revision": "f3e8cbfd5ace99237e90",
     "url": "/tic-tac/static/css/main.a717df1f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/tic-tac/static/css/2.bff108b2.chunk.css"
   },
   {
-    "revision": "2a540457bc00d32023c9572e75e1aeb0",
+    "revision": "882812f12413fb8625ee12e688720646",
     "url": "/tic-tac/index.html"
   }
 ];
